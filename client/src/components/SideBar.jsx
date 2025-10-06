bfb8c34a8b997bd72639cde0d9bdd0f96504aa1a
hirthickroshangk <roshan7hirthick@gmail.com>
2025-10-06 14:55:36 +0530

    Add note to the users.

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -15,6 +15,13 @@ const SideBar = ({ setChatLog }) => {
         <span>+</span>
         New Chat
       </div>
+      <div>
+      Note:
+      This demo website is hosted on a free-tier backend server. Due to the server’s idle timeout policy, the first chat 
+      response may take approximately 20–30 seconds after a period of inactivity, as the server “wakes up.” Subsequent 
+      messages will respond instantly. This is a demo hosted on a free-tier server; full production would respond instantly. 
+      Please allow a few moments for the initial response when testing the chatbot.
+      </div>
     </aside>
   );
 };
